3a4ad1b1c41d91a174dbd2fc3821031f308f2f5e
Anthony Leung <anthoynleung989@gmail.com>
2015-11-25 12:08:57 +1300

    implement api for feedback and get POST working

---
+++ MSANandoso/MSANandoso/Scripts/FeedbackModule.js
@@ -0,0 +1,22 @@
+﻿var FeedbackModuleGet = (function () {
+    return {
+        getFeedbacks: function (callback) {
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "http://localhost:21320/api/Feedbacks",
+                success: function (data) {
+                    callback(data);
+                }
+            });
+        }
+    }
+}());
+
+function postMessage (name, message) {
+    $.ajax({
+        type: "POST",
+        data: { Name: name, Message: message },
+        url: "http://localhost:21320/api/Feedbacks"
+    });
+}

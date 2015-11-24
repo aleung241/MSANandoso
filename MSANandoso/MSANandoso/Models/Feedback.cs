9c096c15f856f58d74e889d8830f1fdcd22bc1ea
Anthony Leung <anthoynleung989@gmail.com>
2015-11-25 12:12:51 +1300

    Revert "Revert "implement api for feedback and get POST working""
    This reverts commit 0c596328e63e91a8a2c8bcf0a86aed377b22cb5a.

---
+++ MSANandoso/MSANandoso/Models/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSANandoso.Models
+{
+	public class Feedback
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Message { get; set; }
+	}
+}

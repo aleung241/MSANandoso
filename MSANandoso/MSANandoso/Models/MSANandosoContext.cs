7d168c5ef91782276d20a30b526cde81072854fa
Anthony Leung <anthoynleung989@gmail.com>
2015-11-25 12:11:21 +1300

    Revert "implement api for feedback and get POST working"
    This reverts commit 3a4ad1b1c41d91a174dbd2fc3821031f308f2f5e.

--- MSANandoso/MSANandoso/Models/MSANandosoContext.cs
+++ MSANandoso/MSANandoso/Models/MSANandosoContext.cs
@@ -10,8 +10,8 @@ namespace MSANandoso.Models
 {
 	[DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
 
-	public class MSANandosoContext : DbContext
-	{
+    public class MSANandosoContext : DbContext
+    {
 
 		public class MyConfiguration : DbMigrationsConfiguration<MSANandosoContext>
 		{
@@ -24,98 +24,60 @@ namespace MSANandoso.Models
 			protected override void Seed(MSANandosoContext context)
 			{
 				var specials = new List<Special>
-					{
-						new Special
-						{
-							Image = "image01.jpg",
-							Title = "Title01",
-							Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum nulla vitae consequat dictum. Fusce blandit, turpis eu lobortis scelerisque, sem purus rutrum magna, sed dapibus diam diam eget ex. Quisque finibus neque eu suscipit venenatis. Vivamus a justo nulla. Ut gravida nunc ligula, eu ultrices arcu aliquet vel. Integer lobortis placerat metus, eget laoreet libero. Etiam non erat eu ipsum rutrum aliquet. Sed a ante bibendum, feugiat ex in, laoreet orci. Aliquam a felis non orci maximus mollis ut id dui. Cras maximus venenatis justo quis tempor. Aenean eu venenatis lorem. Nulla commodo tempor tempor.",
-							Price = 17.99M
-						},
-						new Special
-						{
-							Image = "image02.jpg",
-							Title = "Title02",
-							Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum nulla vitae consequat dictum. Fusce blandit, turpis eu lobortis scelerisque, sem purus rutrum magna, sed dapibus diam diam eget ex. Quisque finibus neque eu suscipit venenatis. Vivamus a justo nulla. Ut gravida nunc ligula, eu ultrices arcu aliquet vel. Integer lobortis placerat metus, eget laoreet libero. Etiam non erat eu ipsum rutrum aliquet. Sed a ante bibendum, feugiat ex in, laoreet orci. Aliquam a felis non orci maximus mollis ut id dui. Cras maximus venenatis justo quis tempor. Aenean eu venenatis lorem. Nulla commodo tempor tempor.",
-							Price = 16.99M
-						},
-						new Special
-						{
-							Image = "image03.jpg",
-							Title = "Title03",
-							Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum nulla vitae consequat dictum. Fusce blandit, turpis eu lobortis scelerisque, sem purus rutrum magna, sed dapibus diam diam eget ex. Quisque finibus neque eu suscipit venenatis. Vivamus a justo nulla. Ut gravida nunc ligula, eu ultrices arcu aliquet vel. Integer lobortis placerat metus, eget laoreet libero. Etiam non erat eu ipsum rutrum aliquet. Sed a ante bibendum, feugiat ex in, laoreet orci. Aliquam a felis non orci maximus mollis ut id dui. Cras maximus venenatis justo quis tempor. Aenean eu venenatis lorem. Nulla commodo tempor tempor.",
-							Price = 15.99M
-						},
-						new Special
-						{
-							Image = "image04.jpg",
-							Title = "Title04",
-							Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum nulla vitae consequat dictum. Fusce blandit, turpis eu lobortis scelerisque, sem purus rutrum magna, sed dapibus diam diam eget ex. Quisque finibus neque eu suscipit venenatis. Vivamus a justo nulla. Ut gravida nunc ligula, eu ultrices arcu aliquet vel. Integer lobortis placerat metus, eget laoreet libero. Etiam non erat eu ipsum rutrum aliquet. Sed a ante bibendum, feugiat ex in, laoreet orci. Aliquam a felis non orci maximus mollis ut id dui. Cras maximus venenatis justo quis tempor. Aenean eu venenatis lorem. Nulla commodo tempor tempor.",
-							Price = 14.99M
-						},
-						new Special
-						{
-							Image = "image05.jpg",
-							Title = "Title05",
-							Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum nulla vitae consequat dictum. Fusce blandit, turpis eu lobortis scelerisque, sem purus rutrum magna, sed dapibus diam diam eget ex. Quisque finibus neque eu suscipit venenatis. Vivamus a justo nulla. Ut gravida nunc ligula, eu ultrices arcu aliquet vel. Integer lobortis placerat metus, eget laoreet libero. Etiam non erat eu ipsum rutrum aliquet. Sed a ante bibendum, feugiat ex in, laoreet orci. Aliquam a felis non orci maximus mollis ut id dui. Cras maximus venenatis justo quis tempor. Aenean eu venenatis lorem. Nulla commodo tempor tempor.",
-							Price = 13.99M
-						}
-					};
-				specials.ForEach(s => context.Specials.AddOrUpdate(p => p.Title, s));
-				context.SaveChanges();
-
-				var feedback = new List<Feedback>
 				{
-					new Feedback
+					new Special
 					{
-						Name = "John",
-						Message = "message 1"
+						Image = "image01.jpg",
+						Title = "Title01",
+						Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum nulla vitae consequat dictum. Fusce blandit, turpis eu lobortis scelerisque, sem purus rutrum magna, sed dapibus diam diam eget ex. Quisque finibus neque eu suscipit venenatis. Vivamus a justo nulla. Ut gravida nunc ligula, eu ultrices arcu aliquet vel. Integer lobortis placerat metus, eget laoreet libero. Etiam non erat eu ipsum rutrum aliquet. Sed a ante bibendum, feugiat ex in, laoreet orci. Aliquam a felis non orci maximus mollis ut id dui. Cras maximus venenatis justo quis tempor. Aenean eu venenatis lorem. Nulla commodo tempor tempor.",
+						Price = 17.99M
 					},
-					new Feedback
+					new Special
 					{
-						Name = "Christine",
-						Message = "message 2"
+						Image = "image02.jpg",
+						Title = "Title02",
+						Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum nulla vitae consequat dictum. Fusce blandit, turpis eu lobortis scelerisque, sem purus rutrum magna, sed dapibus diam diam eget ex. Quisque finibus neque eu suscipit venenatis. Vivamus a justo nulla. Ut gravida nunc ligula, eu ultrices arcu aliquet vel. Integer lobortis placerat metus, eget laoreet libero. Etiam non erat eu ipsum rutrum aliquet. Sed a ante bibendum, feugiat ex in, laoreet orci. Aliquam a felis non orci maximus mollis ut id dui. Cras maximus venenatis justo quis tempor. Aenean eu venenatis lorem. Nulla commodo tempor tempor.",
+						Price = 16.99M
 					},
-					new Feedback
+					new Special
 					{
-						Name = "Ryan",
-						Message = "message 3"
+						Image = "image03.jpg",
+						Title = "Title03",
+						Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum nulla vitae consequat dictum. Fusce blandit, turpis eu lobortis scelerisque, sem purus rutrum magna, sed dapibus diam diam eget ex. Quisque finibus neque eu suscipit venenatis. Vivamus a justo nulla. Ut gravida nunc ligula, eu ultrices arcu aliquet vel. Integer lobortis placerat metus, eget laoreet libero. Etiam non erat eu ipsum rutrum aliquet. Sed a ante bibendum, feugiat ex in, laoreet orci. Aliquam a felis non orci maximus mollis ut id dui. Cras maximus venenatis justo quis tempor. Aenean eu venenatis lorem. Nulla commodo tempor tempor.",
+						Price = 15.99M
 					},
-					new Feedback
+					new Special
 					{
-						Name = "James",
-						Message = "message 4"
+						Image = "image04.jpg",
+						Title = "Title04",
+						Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum nulla vitae consequat dictum. Fusce blandit, turpis eu lobortis scelerisque, sem purus rutrum magna, sed dapibus diam diam eget ex. Quisque finibus neque eu suscipit venenatis. Vivamus a justo nulla. Ut gravida nunc ligula, eu ultrices arcu aliquet vel. Integer lobortis placerat metus, eget laoreet libero. Etiam non erat eu ipsum rutrum aliquet. Sed a ante bibendum, feugiat ex in, laoreet orci. Aliquam a felis non orci maximus mollis ut id dui. Cras maximus venenatis justo quis tempor. Aenean eu venenatis lorem. Nulla commodo tempor tempor.",
+						Price = 14.99M
 					},
-					new Feedback
+					new Special
 					{
-						Name = "Mary",
-						Message = "message 5"
-					},
-					new Feedback
-					{
-						Name = "Bob",
-						Message = "message 6"
+						Image = "image05.jpg",
+						Title = "Title05",
+						Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam interdum nulla vitae consequat dictum. Fusce blandit, turpis eu lobortis scelerisque, sem purus rutrum magna, sed dapibus diam diam eget ex. Quisque finibus neque eu suscipit venenatis. Vivamus a justo nulla. Ut gravida nunc ligula, eu ultrices arcu aliquet vel. Integer lobortis placerat metus, eget laoreet libero. Etiam non erat eu ipsum rutrum aliquet. Sed a ante bibendum, feugiat ex in, laoreet orci. Aliquam a felis non orci maximus mollis ut id dui. Cras maximus venenatis justo quis tempor. Aenean eu venenatis lorem. Nulla commodo tempor tempor.",
+						Price = 13.99M
 					}
 				};
-				feedback.ForEach(s => context.Feedbacks.AddOrUpdate(p => p.Name, s));
+				specials.ForEach(s => context.Specials.AddOrUpdate(p => p.Title, s));
 				context.SaveChanges();
 			}
 		}
 
-		//You can add custom code to this file.Changes will not be overwritten.
-
-		//  If you want Entity Framework to drop and regenerate your database
-		//  automatically whenever you change your model schema, please use data migrations.
-		//  For more information refer to the documentation:
-		//  http://msdn.microsoft.com/en-us/data/jj591621.aspx
-
-		public MSANandosoContext() : base("name=MSANandosoContext")
-		{
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public MSANandosoContext() : base("name=MSANandosoContext")
+        {
 			Database.SetInitializer(new MigrateDatabaseToLatestVersion<MSANandosoContext, MyConfiguration>());
-		}
+        }
 
 		public System.Data.Entity.DbSet<MSANandoso.Models.Special> Specials { get; set; }
-
-		public System.Data.Entity.DbSet<MSANandoso.Models.Feedback> Feedbacks { get; set; }
 	}
 }
